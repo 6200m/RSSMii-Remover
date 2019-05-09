f4bd274a2c5361a4b78f1ef5be510e77ee69e6a0
Andreas Bielawski <brawl98@outlook.com>
2019-05-09 21:46:47 +0200

    Other way around...

--- source/main.c
+++ source/main.c
@@ -13,84 +13,84 @@ nwc24dl_entry nwc24entry;
 
 int main(int argc, char **argv) {
 
-	// Initialise the video system
-	VIDEO_Init();
+    // Initialise the video system
+    VIDEO_Init();
 
-	// This function initialises the attached controllers
-	WPAD_Init();
+    // This function initialises the attached controllers
+    WPAD_Init();
 
-	// Obtain the preferred video mode from the system
-	// This will correspond to the settings in the Wii menu
-	rmode = VIDEO_GetPreferredMode(NULL);
+    // Obtain the preferred video mode from the system
+    // This will correspond to the settings in the Wii menu
+    rmode = VIDEO_GetPreferredMode(NULL);
 
-	// Allocate memory for the display in the uncached region
-	xfb = MEM_K0_TO_K1(SYS_AllocateFramebuffer(rmode));
+    // Allocate memory for the display in the uncached region
+    xfb = MEM_K0_TO_K1(SYS_AllocateFramebuffer(rmode));
 
-	// Initialise the console, required for printf
-	console_init(xfb,20,20,rmode->fbWidth,rmode->xfbHeight,rmode->fbWidth*VI_DISPLAY_PIX_SZ);
+    // Initialise the console, required for printf
+    console_init(xfb,20,20,rmode->fbWidth,rmode->xfbHeight,rmode->fbWidth*VI_DISPLAY_PIX_SZ);
 
-	// Set up the video registers with the chosen mode
-	VIDEO_Configure(rmode);
+    // Set up the video registers with the chosen mode
+    VIDEO_Configure(rmode);
 
-	// Tell the video hardware where our display memory is
-	VIDEO_SetNextFramebuffer(xfb);
+    // Tell the video hardware where our display memory is
+    VIDEO_SetNextFramebuffer(xfb);
 
-	// Make the display visible
-	VIDEO_SetBlack(FALSE);
+    // Make the display visible
+    VIDEO_SetBlack(FALSE);
 
-	// Flush the video register changes to the hardware
-	VIDEO_Flush();
+    // Flush the video register changes to the hardware
+    VIDEO_Flush();
 
-	// Wait for Video setup to complete
-	VIDEO_WaitVSync();
-	if(rmode->viTVMode&VI_NON_INTERLACE) VIDEO_WaitVSync();
+    // Wait for Video setup to complete
+    VIDEO_WaitVSync();
+    if(rmode->viTVMode&VI_NON_INTERLACE) VIDEO_WaitVSync();
 
-	printf("\x1b[2;0H");
-	printf("Welcome to the RSSMii Remover!\n\n");
-	
-	// Initialize WC24
-	printf("Init WiiConnect24...\n");
-	s32 ret = WC24_Init();
-	if (ret < 0) {
-		printf("ERROR: WC24_Init returned %d, exiting\n", ret);
-		exit(0);
-	}
-	
-	// Go through each record with our Title ID and delete it
-	printf("Deleting...\n");
-	int count = 0;
-	while( (ret = WC24_FindRecord((u32)rssmiititleid, &nwc24record)) != LIBWC24_ENOENT) {
-		WC24_DeleteRecord((u32)ret);
-		count++;
-	}
-	printf("Deleted %i entries.\n", count);
-	
-	// Shutdown WC24
-	printf("Shutting down WiiConnect24...\n");
-	ret = WC24_Shutdown();
-	if (ret < 0) {
-		printf("ERROR: WC24_Shutdown returned %d, exiting\n", ret);
-		exit(0);
-	}
-	
-	printf("\nFinished, press HOME to exit!\n");
+    printf("\x1b[2;0H");
+    printf("Welcome to the RSSMii Remover!\n\n");
+    
+    // Initialize WC24
+    printf("Init WiiConnect24...\n");
+    s32 ret = WC24_Init();
+    if (ret < 0) {
+        printf("ERROR: WC24_Init returned %d, exiting\n", ret);
+        exit(0);
+    }
+    
+    // Go through each record with our Title ID and delete it
+    printf("Deleting...\n");
+    int count = 0;
+    while( (ret = WC24_FindRecord((u32)rssmiititleid, &nwc24record)) != LIBWC24_ENOENT) {
+        WC24_DeleteRecord((u32)ret);
+        count++;
+    }
+    printf("Deleted %i entries.\n", count);
+    
+    // Shutdown WC24
+    printf("Shutting down WiiConnect24...\n");
+    ret = WC24_Shutdown();
+    if (ret < 0) {
+        printf("ERROR: WC24_Shutdown returned %d, exiting\n", ret);
+        exit(0);
+    }
+    
+    printf("\nFinished, press HOME to exit!\n");
 
-	while(1) {
-		// Call WPAD_ScanPads each loop, this reads the latest controller states
-		WPAD_ScanPads();
+    while(1) {
+        // Call WPAD_ScanPads each loop, this reads the latest controller states
+        WPAD_ScanPads();
 
-		// WPAD_ButtonsDown tells us which buttons were pressed in this loop
-		// this is a "one shot" state which will not fire again until the button has been released
-		u32 pressed = WPAD_ButtonsDown(0);
-		
-		if (pressed & WPAD_BUTTON_HOME) {
-			printf("Exiting...");
-			exit(0);
-		}
+        // WPAD_ButtonsDown tells us which buttons were pressed in this loop
+        // this is a "one shot" state which will not fire again until the button has been released
+        u32 pressed = WPAD_ButtonsDown(0);
+        
+        if (pressed & WPAD_BUTTON_HOME) {
+            printf("Exiting...");
+            exit(0);
+        }
 
-		// Wait for the next frame
-		VIDEO_WaitVSync();
-	}
+        // Wait for the next frame
+        VIDEO_WaitVSync();
+    }
 
-	return 0;
+    return 0;
 }
